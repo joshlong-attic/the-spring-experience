5061aedcaeeb30effe00c5b3bfc32e007771e3f6
Josh Long <josh.long@springsource.com>
2011-08-03 02:51:47 -0700

    updated code, added a utility script.

---
+++ starting/mvc/src/main/java/org/springsource/examples/config/MvcConfig.java
@@ -0,0 +1,29 @@
+package org.springsource.examples.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@ComponentScan("org.springsource.examples.controllers")
+@Configuration
+@EnableWebMvc
+public class MvcConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void configureResourceHandling(ResourceConfigurer configurer) {
+        configurer.addResourceLocation("/resources/**").addResourceLocations("/resources/");
+    }
+
+    @Bean
+    public InternalResourceViewResolver viewResolver(){
+        InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver ();
+        internalResourceViewResolver.setPrefix("/WEB-INF/views/");
+        internalResourceViewResolver.setSuffix(".jsp");
+        return internalResourceViewResolver;
+    }
+
+
+}
