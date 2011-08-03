f966e4e99fac567e3c30643068f87db3b723be50
Josh Long <josh.long@springsource.com>
2011-08-03 03:04:11 -0700

    moving jquery and resources

--- starting/mvc/src/main/java/org/springsource/examples/config/MvcConfig.java
+++ starting/mvc/src/main/java/org/springsource/examples/config/MvcConfig.java
@@ -3,6 +3,7 @@ package org.springsource.examples.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -15,6 +16,11 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void configureResourceHandling(ResourceConfigurer configurer) {
         configurer.addResourceLocation("/resources/**").addResourceLocations("/resources/");
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
     }
 
     @Bean
