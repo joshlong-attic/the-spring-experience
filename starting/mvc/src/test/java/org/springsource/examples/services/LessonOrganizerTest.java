c44795284c169d9de95ed45798159fe42258d322
Josh Long <josh.long@springsource.com>
2011-08-03 23:32:55 -0700

    added tests.

---
+++ starting/mvc/src/test/java/org/springsource/examples/services/LessonOrganizerTest.java
@@ -0,0 +1,34 @@
+package org.springsource.examples.services;
+
+import junit.framework.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springsource.examples.config.ServicesConfig;
+import org.springsource.examples.domain.Lesson;
+import org.springsource.examples.domain.LessonPlan;
+
+import javax.inject.Inject;
+
+/**
+ * @author Josh Long
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = ServicesConfig.class)
+public class LessonOrganizerTest {
+
+    @Inject
+    private LessonOrganizer lessonOrganizer;
+
+    @Test
+    public void testLessonOrganizer() throws Throwable {
+
+        Lesson  lesson = lessonOrganizer.createLesson(
+                lessonOrganizer.createLessonPlan().getId(), 2, "Getting Started with Spring");
+        LessonPlan lessonPlan = lessonOrganizer.getLessonPlanById(lesson.getLessonPlan().getId());
+
+        Assert.assertNotNull( lesson.getLessonPlan());
+        Assert.assertTrue( lessonPlan.getLessons().size()  > 0);
+    }
+}
